7521ce7dad0fc1d7903d66fd8cd5b0c114e34ca1
Iqbal Bin Mohamad Zainuddin <iqbalz.dev@gmail.com>
2021-01-31 22:40:18 +0800

    Completed CRUD

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -3,7 +3,7 @@ import TextInputGroup from '../layout/TextInputGroup';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addContact } from '../../actions/contactActions';
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 
 class AddContact extends Component {
   state = {
@@ -35,7 +35,7 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
+      // id: uuid(),
       name,
       email,
       phone
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,33 +1,46 @@
-import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from '../actions/type';
+import { 
+    GET_CONTACTS, 
+    DELETE_CONTACT, 
+    ADD_CONTACT,
+    GET_CONTACT,
+    UPDATE_CONTACT
+} from '../actions/type';
 
 const initialState = {
     contacts: [
-        {
-          id: 1,
-          name: 'John Doe',
-          email: 'john@gmail.com',
-          phone: '555-555-5555'
-        },
-        {
-          id: 2,
-          name: 'Karen Gilan',
-          email: 'karen@gmail.com',
-          phone: '444-444-4444'
-        },
-        {
-          id: 3,
-          name: 'Henry Johnson',
-          email: 'henry@gmail.com',
-          phone: '333-333-333'
-        }
-      ]
+        // {
+        //   id: 1,
+        //   name: 'John Doe',
+        //   email: 'john@gmail.com',
+        //   phone: '555-555-5555'
+        // },
+        // {
+        //   id: 2,
+        //   name: 'Karen Gilan',
+        //   email: 'karen@gmail.com',
+        //   phone: '444-444-4444'
+        // },
+        // {
+        //   id: 3,
+        //   name: 'Henry Johnson',
+        //   email: 'henry@gmail.com',
+        //   phone: '333-333-333'
+        // }
+      ],
+      contact: {}
 };
 
 export default function contactReducer(state = initialState, action) {
     switch (action.type) {
         case GET_CONTACTS:
             return {
-                ...state
+                ...state,
+                contacts: action.payload
+            }
+        case GET_CONTACT:
+            return {
+                ...state,
+                contact: action.payload
             }
         case DELETE_CONTACT:
             return {
@@ -38,6 +51,15 @@ export default function contactReducer(state = initialState, action) {
             return {
                 ...state,
                 contacts: [action.payload, ...state.contacts]
+            }
+        case UPDATE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.map(contact => 
+                    contact.id === action.payload.id 
+                    ? (contact = action.payload) 
+                    : contact
+                )
             }
         default:
             return state;
